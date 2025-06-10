5e4eb8e383c2fdcba80fa3cc0e301aaf17d8097e
kyoh86 <me@kyoh86.dev>
2025-06-10 23:40:33 +0900

    Update dependencies

--- denops/qftable/flag.ts
+++ denops/qftable/flag.ts
@@ -1,4 +1,4 @@
-import type { Flags } from "jsr:@denops/std@7.5.0/argument";
+import type { Flags } from "jsr:@denops/std@7.5.1/argument";
 
 export function getFlagValue(flags: Flags, key: string): string | undefined {
   if (!(key in flags)) {
